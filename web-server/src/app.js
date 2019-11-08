cff5330b8951dbd585bb9049830bd5bf2e7cf210
Denner Fernandes <dev.denner@outlook.com>
2019-11-07 21:48:49 -0300

    [feat]: aula hbs
    Signed-off-by: Denner Fernandes <dev.denner@outlook.com>

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,10 +1,38 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index',{
+        title: 'Cotações',
+        author: 'Biharck'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about',{
+        title: 'Sobre',
+        author: 'Biharck Araújo'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help',{
+        title: 'Ajuda',
+        author: 'Biharck Araújo'
+    })
+})
 
 app.get('/cotacoes', (req, res) => {
     const cotacao = {
@@ -20,6 +48,23 @@ app.get('/cotacoes', (req, res) => {
     cotacoes.push(cotacao)
 
     res.send(cotacoes)
+})
+
+app.get('/help/*', (req, res) => {
+    
+    res.render('404', {
+        title : '404',
+        errorMessage : 'Não existe página depois de /help',
+        author: 'Biharck Araújo'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title : '404',
+        errorMessage : 'Página não encontrada',
+        author: 'Biharck Araújo'
+    })
 })
 
 app.listen(3000, () => {
